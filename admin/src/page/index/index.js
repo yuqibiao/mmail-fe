7d4f10798807826fe16dabdc577f178a20fb8150
yuqibiao <597197690@qq.com>
2018-01-12 18:19:30 +0800

    init commit

--- admin/src/page/index/index.js
+++ admin/src/page/index/index.js
@@ -5,16 +5,42 @@
  */
 
 require('./index.css');
-layui.use(['bodyTab','form','element','layer','jquery'], function(){
-    var form = layui.form;
+var tab;
+var form;
+layui.use(['bodyTab', 'form', 'element', 'layer', 'jquery'], function () {
+    form = layui.form;
     //监听提交
     /*form.on('submit(formDemo)', function(data){
-        layer.msg(JSON.stringify(data.field));
-        return false;
-    });*/
-    var tab = layui.bodyTab({
-        openTabNum : "50",  //最大可打开窗口数量
-        url : "../plugin/layui/json/navs.json" //获取菜单json地址
+     layer.msg(JSON.stringify(data.field));
+     return false;
+     });*/
+    tab = layui.bodyTab({
+        openTabNum: "50",  //最大可打开窗口数量
+        url: "../plugin/layui/json/navs.json" //获取菜单json地址
     });
     tab.render();
+
+    //隐藏左侧导航
+    $(".left-menu-switch").click(function () {
+        console.log("==========")
+        $(".layui-layout-admin").toggleClass("showMenu");
+        //渲染顶部窗口
+        tab.tabMove();
+    })
+
+    // 添加新窗口
+    $("body").on("click", ".layui-nav .layui-nav-item a", function () {
+        //如果不存在子级
+        if ($(this).siblings().length == 0) {
+            addTab($(this));
+            $('body').removeClass('site-mobile');  //移动端点击菜单关闭菜单层
+        }
+        $(this).parent("li").siblings().removeClass("layui-nav-itemed");
+    })
+
 });
+
+//打开新窗口
+function addTab(_this) {
+    tab.tabAdd(_this);
+}
